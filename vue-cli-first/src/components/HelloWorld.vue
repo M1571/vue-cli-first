b80ae5ff6d505484773b55b6192c99a9fcb39ca2
M1571 <94639639+M1571@users.noreply.github.com>
2022-03-08 01:59:38 +0100

    added title e hero

--- vue-cli-first/src/components/HelloWorld.vue
+++ vue-cli-first/src/components/HelloWorld.vue
@@ -16,6 +16,9 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+h1 {
+  border: 3px solid royalblue;
+}
 h3 {
   margin: 40px 0 0;
 }
