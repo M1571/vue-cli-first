e25b72a9450ffaa2c8d86afd9f7b9b0506fc1f12
M1571 <94639639+M1571@users.noreply.github.com>
2022-03-08 02:21:44 +0100

    added jumbo

--- vue-cli-first/src/App.vue
+++ vue-cli-first/src/App.vue
@@ -3,16 +3,20 @@
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld/>
     <h1>La mia prima app con Vue CLI!</h1>
+    <h2>Sono un Jumbotron</h2>
+    <MyGallery/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import MyGallery from '.components/MyGallery.vue'
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+    MyGallery
   }
 }
 </script>
---
+++ vue-cli-first/src/components/MyGallery.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="gallery">
+    <img v-for="(slide, index) in slides" src="../assets/logo.png" :key="index">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MyGallery',
+  data() {
+    return {
+      slides: ["slide1", "slide2", "slide3"]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h1 {
+  border: 3px solid royalblue;
+}
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
